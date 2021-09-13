330e4bceb20c0ce1299f26afbab4bbaea1652fae
Chris <chris@chris-agnew.com>
2021-09-13 18:09:33 -0400

    convert to Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "medium-blue": "#093F68",
+        "dark-blue": "#080C20",
+        "error-red": "#FF2965",
+        "light-blue": "#3EE9E5",
+        "text-gray": "#777F98",
+      },
+      font: {
+        Manrope: ["Manrope"],
+      },
+      backgroundImage: {
+        "email-pattern": "url('/assets/bg-footer-squiggle.svg')",
+        "hero-pattern": "url('/assets/bg-hero-squiggle.svg')",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
